fedffa6278b343ff3d201374b4110b6bd4cae710
Özlen Yudum Besleme <yudum.besleme@hubx.co>
2025-10-08 15:23:57 +0300

    ios updates

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,9 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+  // Google Maps API Key
+  GMSServices.provideAPIKey("AIzaSyDCGHaPLh0xtssZETBIq6MnyQbwV0rzKVM")
+  
   if #available(iOS 10.0, *) {
   UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
   }
